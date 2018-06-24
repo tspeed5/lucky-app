c28c85ea6707f6bf189175d42ca793a6167881dc
Tracy Speed <tspeed@me.com>
2018-06-24 12:13:26 -0500

    notes

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,2 +1,9 @@
 class Api::PagesController < ApplicationController
+
+  def fortunes_action
+    fortunes = ["Never forget a friend. Especially if he owes you.", "That wasn’t chicken.", "You love Chinese food."]  
+    
+    @message = fortunes.sample
+    render "fortunes_view.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  get "/fortunes_url" => "api/pages#fortunes_action"
 end
